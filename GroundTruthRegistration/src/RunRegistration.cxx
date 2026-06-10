3a3ea26357b3d16c4894e700deae29610f6aacac
JakeKitzmann <97077033+JakeKitzmann@users.noreply.github.com>
2026-06-10 08:40:40 -0500

    beginnings of C++ registration pipeline

---
+++ GroundTruthRegistration/src/RunRegistration.cxx
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <itkImage.h>
+
+template <typename TPixel>
+auto Process(){
+    std::cout << "Process" << std::endl;
+}
+
+int main(int argc, const char* argv[])
+{  
+
+    PARSE_ARGS;
+
+    auto ImageIO
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
